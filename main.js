86f6a536833706afe341f745e3891a2d3e34b98b
sergBull <bullsergv@gmail.com>
2020-01-01 15:47:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var acx = new AudioContext();
 var ctx = document.querySelector("canvas").getContext("2d");
 
-var osc = acx.createOscillator();
-osc.frequency.value = 440;
-osc.type = "square";
-osc.connect(acx.destination);
+window.document.body.children[0]..onclick = function(e) {
+	var acx = new AudioContext();var osc = acx.createOscillator();
+	osc.frequency.value = 440;
+	osc.type = "square";
+	osc.connect(acx.destination);
 
-osc.start();
-setTimeout(function() {
-	osc.stop();
-	osc = null;
-	ctx.fillRect(canvas.width / 2, canvas.height / 2, canvas.width / 4, canvas.height / 4);
-}, 500);
+	osc.start();
+	setTimeout(function() {
+		osc.stop();
+		osc = null;
+		ctx.fillStyle = "rgba(255, 127, 63)";
+		ctx.fillRect(ctx.canvas.width / 2, ctx.canvas.height / 2, ctx.canvas.width / 4, ctx.canvas.height / 4);
+	}, 500);
+};
