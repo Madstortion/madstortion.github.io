c5c4c8ee3ed2b31646a315cda1ba9652d65f37a4
sergBull <bullsergv@gmail.com>
2020-01-01 15:35:24 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var acx = new AudioContext();
+var ctx =document.querySelector("canvas").getContext("2d");
+
+var osc = acx.createOscillator();
+osc.frequncy.value = 440;
+osc.type = "square";
+osc.connect(ctx.destination);
+
+osc.start();
+setTimeout(function() {
+	osc.stop();
+	osc = null;
+	ctx.fillRect(canvas.width / 2, canvas.height / 2, canvas.width / 4, canvas.height / 4);
+}, 500);
