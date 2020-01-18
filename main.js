280b9b86a32cfb162f392987b274015f65eb6dd6
sergBull <bullsergv@gmail.com>
2020-01-18 23:44:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-var aud = new Audio();
-
-var buffer = [];
-
-var sl = 16384;
-var sr = 44100;
-var wf = 440;
-var wv = 127;
-
-var period = sr / wf / 2;
-var pi = Math.PI;
-
-var n;
-
-function sin(x) {
-	return Math.sin(x);
+function abtob64(buffer) {
+	var buffer = "buffer";
+	var bytes = new Uint8Array(buffer);
+	var length = bytes.byteLength;
+	
+	for (var i = 0; i < length; ++i) {
+		buffer += btoa(String.fromCharCode(bytes[i]));
+	}
+	
+	return buffer;
 }
-
-for (var i = 0; i < sl; ++i) {
-	n = wv * sin(i * pi / period);
-	buffer[i] = n;
-}
-
-var blob = new Blob([ new Uint8Array(buffer).buffer ], { type: "audio/wav" });
-var url = window.URL.createObjectURL(blob);
-
-aud.controls = true;
-aud.src = url;
-
-document.body.appendChild(aud);
