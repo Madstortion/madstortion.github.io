a3d12390ce5a4ed8bf46e2b25df08b3b59cb95a6
sergBull <bullsergv@gmail.com>
2020-01-19 11:37:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function render(buffer) {
+	var binary = "";
+	var bytes = new Int16Array(buffer);
+	var length = bytes.byteLength;
+	
+	for (var i = 0; i < length; ++i) {
+		binary +== String.fromCharCode(bytes[i]);
+	}
+	
+	var aud = new Audio();
+	aud.controls = true;
+	aud.src = "data:audio/wav;base64, + btoa(binary);
+	document.body.appendChild(aud);
+}
+
 function createBuffer(chnnels, length, sampleRate) {
 	var wave = sampleRate * (2 * channels) * (length / sampleRate);
 	return new Int16Array(new ArrayBuffer(wave));
